a7b6d9bc11f9f55cb2847fe0870aea8e95b011a6
wezerg <vivian.grimaux@hotmail.fr>
2022-12-30 20:51:31 +0100

    Add welcome message for landing page

--- spa/chat-app-frontend/src/views/HomeView.vue
+++ spa/chat-app-frontend/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { useAuthService } from '../services/authService';
 
+const {user} = useAuthService();
 </script>
 
 <template>
-    
+    <h1>Chat application</h1>
+    <p v-if="!user">Veuillez vous connecté pour continuer</p>
+    <p v-else>Rebonjour à vous {{ user.username }}</p>
 </template>
