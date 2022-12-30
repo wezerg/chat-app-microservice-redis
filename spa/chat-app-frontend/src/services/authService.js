fa824377368cf1e44b42c3b707e32417c949b4e6
wezerg <vivian.grimaux@hotmail.fr>
2022-12-30 19:09:57 +0100

    Add disconnect function to service auth

--- spa/chat-app-frontend/src/services/authService.js
+++ spa/chat-app-frontend/src/services/authService.js
@@ -1,7 +1,11 @@
 import { ref } from "vue";
 import instAxios from "./InstAxios";
+import cookies from 'vue-cookies';
+import { useRouter } from "vue-router";
 
 const user = ref(null);
+    
+const router = useRouter();
 
 async function login(username, password){
     const payload = {username, password};
@@ -17,8 +21,14 @@ async function loginAuto(){
     return await instAxios().get("http://localhost:3000/auth/auto").catch(error => error.response);
 }
 
+function disconnect(){
+    user.value = null;
+    cookies.remove('auth-chat-app');
+    router.push({ name: "home" });
+}
+
 function useAuthService(){
-    return {user, login, register, loginAuto};
+    return {user, login, register, loginAuto, disconnect};
 }
 
 export {useAuthService};
