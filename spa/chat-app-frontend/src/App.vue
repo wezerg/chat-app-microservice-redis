86c3480ab146d807ad6e246b23e8b77abc5e01e2
wezerg <vivian.grimaux@hotmail.fr>
2022-12-30 19:43:52 +0100

    Add auto connection to front-end app

--- spa/chat-app-frontend/src/App.vue
+++ spa/chat-app-frontend/src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
+import { useAuthService } from './services/authService';
+
+const {user, loginAuto} = useAuthService();
+
+onMounted(async () => {
+    const {status, data} = await loginAuto();
+    if (status === 200) {
+        user.value = data;
+    }
+});
 </script>
 
 <template>
