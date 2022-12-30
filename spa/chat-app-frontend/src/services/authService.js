533c75a52c727eca595d2e27b5925c22d4347793
wezerg <vivian.grimaux@hotmail.fr>
2022-12-30 19:01:49 +0100

    Open services folder - Create authentication service - Create instance axios for requesting

---
+++ spa/chat-app-frontend/src/services/authService.js
@@ -0,0 +1,24 @@
+import { ref } from "vue";
+import instAxios from "./InstAxios";
+
+const user = ref(null);
+
+async function login(username, password){
+    const payload = {username, password};
+    return await instAxios().post("http://localhost:3000/auth/login", payload).catch(error => error.response);
+}
+
+async function register(username, password, confirmPassword){
+    const payload = {username, password, confirmPassword}
+    return await instAxios().post("http://localhost:3000/auth/register", payload).catch(error => error.response);
+}
+
+async function loginAuto(){
+    return await instAxios().get("http://localhost:3000/auth/auto").catch(error => error.response);
+}
+
+function useAuthService(){
+    return {user, login, register, loginAuto};
+}
+
+export {useAuthService};
