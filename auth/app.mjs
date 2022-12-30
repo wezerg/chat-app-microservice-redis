7f3205201f46082835ccabce1fd43b0051ef4f29
wezerg <vivian.grimaux@hotmail.fr>
2022-12-30 20:41:05 +0100

    Add Redis in register route

--- auth/app.mjs
+++ auth/app.mjs
@@ -1,12 +1,14 @@
 import { createClient } from 'redis';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import crypt from 'bcrypt';
 
 const app = express();
 
 const client = createClient({url: 'redis://redis'});
 client.on('error', (err) => console.log('Redis Client Error', err));
 await client.connect();
+await client.SET('incrementalid', 0);
 
 app.use(express.json()); // Middleware Express Json
 app.use(cookieParser()); // Middleware Express Json
@@ -28,13 +30,22 @@ app.post('/auth/login', async (req, res, next) => {
 app.post('/auth/register', async (req, res, next) => {
     if (req.body.username && req.body.password && req.body.confirmPassword) {
         if (req.body.password === req.body.confirmPassword) {
-            /**
-             * Crypte mot de passe
-             * Incrémenter le SET "incremental_id" pour avoir un nouvel id
-             * Créer une nouvelle entrée dans HASH users de redis {username: username, password: crypt(password), idSession: incremental_id}
-             * Renvoyer son idSession dans le cookie à la place de "Hello world"
-             */
-            res.status(200).cookie('auth-chat-app', 'Hello world').send({username: req.body.username});
+            const existingUser = parseInt(await client.EXISTS(`users:${req.body.username}`));
+            if (!existingUser) {
+                const idIncrement = await client.INCR('incrementalid', 1);
+                if (idIncrement) {
+                    const salt = await crypt.genSalt(10);
+                    const passwordHash = await crypt.hash(req.body.password, salt);
+                    const hash = await client.HSET(`users:${idIncrement}`, {username: req.body.username, password: passwordHash});
+                    if (hash) {
+                        await client.SET(`users:${req.body.username}`, '1');
+                        res.status(200).cookie('auth-chat-app', idIncrement).send({username: req.body.username});   
+                    }          
+                }                
+            }
+            else{
+                next('Un utilisateur avec ce pseudo existe déja');
+            }
         }
         else{
             next("Le mot de passe et sa confirmation ne correspondent pas");
