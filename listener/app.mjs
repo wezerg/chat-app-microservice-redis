076cb251130cf0b53029fd1b1bd8e0cb50767546
wezerg <vivian.grimaux@hotmail.fr>
2022-12-31 16:09:06 +0100

    Service listener

---
+++ listener/app.mjs
@@ -0,0 +1,23 @@
+import { WebSocketServer } from 'ws';
+import { createClient } from 'redis';
+
+const client = createClient({url: 'redis://redis'});
+client.on('error', (err) => console.log('Redis Client Error', err));
+const subscriber = client.duplicate();
+subscriber.on('error', (err) => console.log('Redis Client Error', err));
+
+await client.connect();
+await subscriber.connect();
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', async function connection(ws, http) {
+    await subscriber.subscribe('messages', async (message) => {
+        const data = JSON.parse(message);
+        const messages = await client.LRANGE(`messages:${data.roomId}`, 0, -1);
+        for (let i = 0; i < messages.length; i++) {
+            messages[i] = JSON.parse(messages[i]);
+        }
+        ws.send(JSON.stringify(messages)); // Send to all websocket connection, ignoring roomid
+    });
+});
