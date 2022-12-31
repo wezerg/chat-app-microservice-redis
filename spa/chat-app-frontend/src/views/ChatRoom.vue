a4974d1cd1b20817cc4b23410d2c1ebf613bea0e
wezerg <vivian.grimaux@hotmail.fr>
2022-12-31 12:29:49 +0100

    Call endpoint to get one room

--- spa/chat-app-frontend/src/views/ChatRoom.vue
+++ spa/chat-app-frontend/src/views/ChatRoom.vue
@@ -1,12 +1,22 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
+import instAxios from '../services/InstAxios';
 
 const router = useRouter();
 const { id } = router.currentRoute.value.params;
+
+const room = ref([]);
+onMounted(async () => {
+    const {status, data} = await instAxios().get(`/chatrooms/${id}`).catch(error => error.response);
+    if (status === 200) {
+        room.value = data;
+    }
+});
 </script>
 <template>
     <div>
-        Hello Chat room n°{{ id }}
+        <h1>Bienvenue dans le salon {{ room.name }}</h1>
     </div>
 </template>
 <style>
