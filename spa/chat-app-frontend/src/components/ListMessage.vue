c8f3a2912c2473b72759b2be589b52e99b0605ed
wezerg <vivian.grimaux@hotmail.fr>
2023-01-01 16:52:47 +0100

    CSS Style of chatbox

--- spa/chat-app-frontend/src/components/ListMessage.vue
+++ spa/chat-app-frontend/src/components/ListMessage.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { defineProps, ref, onMounted, toRefs } from 'vue';
+import { useAuthService } from '../services/authService';
 
 const messages = ref([]);
+const { user } = useAuthService();
 const props = defineProps({
     roomId: {
         type: String,
@@ -25,14 +27,52 @@ onMounted(async () => {
 <template>
     <div class="chatbox-messages">
         <ul v-if="messages && messages.length">
-            <li v-for="m of messages">{{ m.text }}</li>
+            <li v-for="m of messages" :class="user.id !== m.userId ? 'card-messages' : 'card-messages-own'">
+                <span class="messages-user">{{ m.username }}</span>
+                <div>{{ m.text }}</div>
+                <span class="messages-date">{{ new Date(m.date) }}</span>
+            </li>
         </ul>
     </div>
 </template>
 <style>
 .chatbox-messages{
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
     flex-grow: 1;
+}
+ul{
+    padding: 0 8px;
+    max-height: 700px;
     overflow-y: auto;
-    margin-bottom: 16px;
+    display: flex;
+    flex-direction: column-reverse;
+}
+li[class^="card-messages"]{
+    list-style-type: none;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+}
+li[class^="card-messages"] > div{
+    list-style-type: none;
+    box-shadow: 0 0 4px 2px #565964;
+    max-width: 45%;
+    border-radius: 5px;
+    padding: 8px;
+}
+li[class^="card-messages"] > .messages-user{
+    color: #5c71bd;
+}
+li[class^="card-messages"] > .messages-date{
+    color: #565964;
+    font-size: 12px;
+}
+li.card-messages-own{
+    align-items: flex-end;
+}
+li.card-messages-own > .messages-user{
+    color: #bd805c;
 }
 </style>
