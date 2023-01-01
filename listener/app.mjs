c63c0775d40ee90b79458be7008099ab2c8fcf22
wezerg <vivian.grimaux@hotmail.fr>
2023-01-01 16:55:41 +0100

    Add messages while connect to Websocket

--- listener/app.mjs
+++ listener/app.mjs
@@ -12,6 +12,12 @@ await subscriber.connect();
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', async function connection(ws, http) {
+    const messages = await client.LRANGE(`messages:${http.url.replace('/listener/', '')}`, 0, -1);
+    for (let i = 0; i < messages.length; i++) {
+        messages[i] = JSON.parse(messages[i]);
+    }
+    ws.send(JSON.stringify(messages));
+
     await subscriber.subscribe('messages', async (message) => {
         const data = JSON.parse(message);
         if (http.url.replace('/listener/', '') === data.roomId) {
