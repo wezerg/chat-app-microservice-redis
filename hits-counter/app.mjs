f4f5c005c4ab5b3bb58cfff86ff5214c48c86611
wezerg <vivian.grimaux@hotmail.fr>
2023-01-20 16:23:33 +0100

    Correction cors policies

--- hits-counter/app.mjs
+++ hits-counter/app.mjs
@@ -9,12 +9,7 @@ client.on('error', (err) => console.log('Redis Client Error', err));
 await client.connect();
 
 app.use(express.json()); // Middleware Express Json
-app.use(cors()); // Middleware Cors
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({origin: "*"})); // Middleware Cors
 
 app.get('/hits', async (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // Get IP Adress of client
